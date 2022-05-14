afd07a4ac2850e452966b5199190186b590910ee
judah <87juda@gmail.com>
2022-05-14 17:19:23 -0300

    Initial redux structure

---
+++ src/app/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../index.css';
+
+export const Navbar = () => {
+  return (
+    <div className="WebHeader">
+      <h1 className="HeaderTitle">
+        Bookstore (redux version)
+      </h1>
+      <div className="navContent">
+        <div className="navLinks"></div>
+      </div>
+    </div>
+  )
+}
