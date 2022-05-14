7fe04dd1e27bac732c2f57c3bc538cdf106f0bd9
judah <87juda@gmail.com>
2022-05-14 17:45:53 -0300

    Fix linter typos

--- src/features/books/AddBookForm.js
+++ src/features/books/AddBookForm.js
@@ -15,7 +15,7 @@ export const AddBookForm = () => {
   const dispatch = useDispatch();
 
   const onTitleChanged = (e) => setTitle(e.target.value);
-  const onAuthorChanged =(e) => setAuthor(e.target.value);
+  const onAuthorChanged = (e) => setAuthor(e.target.value);
 
   const onSavePostClicked = () => {
     if (title && author) {
@@ -24,39 +24,39 @@ export const AddBookForm = () => {
           id: nanoid(),
           title,
           author,
-        })
+        }),
       );
 
       setTitle('');
       setAuthor('');
     }
-  }
+  };
 
   return (
-    <section>
-      <form>
-        <input
-          type="text"
-          className="add-book-text"
-          placeholder="Add a Title"
-          id="postTitle"
-          name="postTitle"
-          value={title}
-          onChange={onTitleChanged}
-        />
-        <input
-          type="text"
-          className="add-book-text"
-          placeholder="Add an Author"
-          id="bookAuthor"
-          name="bookAuthor"
-          value={author}
-          onChange={onAuthorChanged}
-        />
-        <button type="button" onClick={onSavePostClicked}>
-            Add book
-        </button>
-      </form>
-    </section>
+    <form className="add-book-container">
+      <input
+        type="text"
+        className="add-book-text"
+        placeholder="Add a Title"
+        id="postTitle"
+        name="postTitle"
+        value={title}
+        onChange={onTitleChanged}
+      />
+      <input
+        type="text"
+        className="add-book-text"
+        placeholder="Add an Author"
+        id="bookAuthor"
+        name="bookAuthor"
+        value={author}
+        onChange={onAuthorChanged}
+      />
+      <button type="button" onClick={onSavePostClicked}>
+        Add
+      </button>
+    </form>
   );
 };
+
+export default AddBookForm;
