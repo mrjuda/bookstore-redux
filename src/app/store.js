3cb571b0eb2d5639bfb366e1dcdf708f5b5bf33f
judah <87juda@gmail.com>
2022-05-14 17:17:59 -0300

    Initial commit

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import booksReducer from '../features/books/booksSlice';
-
-export default configureStore({
-  reducer: {
-    books: booksReducer,
-  },
-});
