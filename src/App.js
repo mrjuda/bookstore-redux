5df4f407b69f6a6101909a00459c453ab914b876
judah <87juda@gmail.com>
2022-05-14 22:44:49 -0300

    Fix linter typos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import {
-  Navigate,
   Routes,
   BrowserRouter as Router,
-  Switch,
   Route,
-  Redirect,
 } from 'react-router-dom';
 
 import { Navbar } from './app/Navbar';
@@ -16,23 +13,23 @@ import { WebCategories } from './components/pages/WebCategories';
 function App() {
   return (
     <div className="container">
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route
-          exact
-          path="/"
-          element = {(
-            <>
-              <BooksList />
-              <AddBookForm />
-            </>
-          )}
-        />
-        {/* <Redirect to="/" /> */}
-        <Route path="/WebCategories" element={<WebCategories />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={(
+              <>
+                <BooksList />
+                <AddBookForm />
+              </>
+            )}
+          />
+          {/* <Redirect to="/" /> */}
+          <Route path="/WebCategories" element={<WebCategories />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
